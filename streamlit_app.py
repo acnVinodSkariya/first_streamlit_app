b234ebcf1bdd74f27ec80d368a52a79acaec3e9c
acnVinodSkariya <126423910+acnVinodSkariya@users.noreply.github.com>
2023-02-26 17:17:19 -0500

    Update streamlit_app.py
    Added new menu items and icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.title('My Parents New Health Diner')
 st.header('Breakfest Menu')
 
-st.subheader('Omega 3 & Blueberry Oatmeal')
-st.subheader('Kale, Spinish & Rocket Smoothie')
-st.subheader('Hard-bolied Free-range Eggs')
+st.text('🥣Omega 3 & Blueberry Oatmeal')
+st.text('🥗 Kale, Spinish & Rocket Smoothie')
+st.text('🐔 Hard-bolied Free-range Eggs')
+st.text('🥑🍞 Avacado Toast')
