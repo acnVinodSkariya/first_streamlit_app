5e94540a517bda9f8a501f94014a5c71be6f3ee7
acnVinodSkariya <126423910+acnVinodSkariya@users.noreply.github.com>
2023-02-26 19:50:03 -0500

    Update streamlit_app.py
    added title and json decode

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,5 +20,7 @@ fruit_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), 
 fruits_to_show = my_fruit_list.loc[fruit_selected]
 st.dataframe(fruits_to_show)
 
+
+st.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
